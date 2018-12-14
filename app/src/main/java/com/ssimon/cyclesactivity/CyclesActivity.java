f859c7d7a4e7fc6ffe80de47366026cee05fd7e3
Steve <stevehs17@gmail.com>
2018-12-13 18:51:35 -0800

    Initial checkin. In progress: creating layouts.

---
+++ app/src/main/java/com/ssimon/cyclesactivity/CyclesActivity.java
@@ -0,0 +1,13 @@
+package com.ssimon.cyclesactivity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CyclesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.cycles_activity);
+    }
+}
