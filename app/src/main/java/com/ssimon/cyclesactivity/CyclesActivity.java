2b59b9dc5b51f52d3a2a6bc7774967cd340f0cb6
Steve <stevehs17@gmail.com>
2019-01-19 16:42:00 -0800

    In progress: switching from TextViews to Buttons for cycle parameter fields.

--- app/src/main/java/com/ssimon/cyclesactivity/CyclesActivity.java
+++ app/src/main/java/com/ssimon/cyclesactivity/CyclesActivity.java
@@ -2,12 +2,21 @@ package com.ssimon.cyclesactivity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class CyclesActivity extends AppCompatActivity {
+    static final private String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.cycles_activity);
     }
+
+    public void onClickVacuumSecs(View v) {
+        Log.v(TAG, "clicked");
+    }
 }
+
+
