daf9bb7cdfb7638a302d37a12dba98b98e63263b
Steve <stevehs17@gmail.com>
2019-01-28 14:58:30 -0800

    Have successfully implemented long touch action for decrement button. Will provide analogous code for increment button. Will then refactor and clean up code.

--- app/src/main/java/com/ssimon/cyclesactivity/CyclesActivity.java
+++ app/src/main/java/com/ssimon/cyclesactivity/CyclesActivity.java
@@ -1,10 +1,14 @@
 package com.ssimon.cyclesactivity;
 
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewConfiguration;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.SeekBar;
 import android.widget.TableLayout;
 import android.widget.TextView;
@@ -28,6 +32,59 @@ public class CyclesActivity extends AppCompatActivity {
         maxValueText = (TextView) findViewById(R.id.txt_max_value);
         seekBar = (SeekBar) findViewById(R.id.seek);
         parmTable = (TableLayout) findViewById(R.id.lay_parms);
+
+        final ImageButton decr = (ImageButton) findViewById(R.id.btn_decrement);
+        final Runnable r = new Runnable() {
+            @Override
+            public void run() {
+                Log.v(TAG, "big decrement");
+                if (currentParmValues == null)
+                    return;
+                int idx = seekBar.getProgress();
+                if (idx >= 5) {
+                    idx -= 5;
+                    setSeekBar(idx);
+                }
+                decr.postDelayed(this, ViewConfiguration.getLongPressTimeout());
+            }
+        };
+        decr.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent e) {
+                if (e.getAction() == MotionEvent.ACTION_DOWN) {
+                    Log.v(TAG, "decrement");
+                    onClickDecrement(null);
+                    v.postDelayed(r, ViewConfiguration.getLongPressTimeout());
+                } else if (e.getAction() == MotionEvent.ACTION_MOVE || e.getAction() == MotionEvent.ACTION_UP) {
+                    Log.v(TAG, "removing callback...");
+                    v.removeCallbacks(r);
+                }
+                return true;
+            }
+        });
+        /*
+        final ImageButton decr = (ImageButton) findViewById(R.id.btn_decrement);
+        final Runnable r = new Runnable() {
+            @Override
+            public void run() {
+                Log.v(TAG, "big decrement");
+                decr.postDelayed(this, ViewConfiguration.getLongPressTimeout());
+            }
+        };
+        decr.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent e) {
+                if (e.getAction() == MotionEvent.ACTION_DOWN) {
+                    Log.v(TAG, "decrement");
+                    v.postDelayed(r, ViewConfiguration.getLongPressTimeout());
+                } else if (e.getAction() == MotionEvent.ACTION_MOVE || e.getAction() == MotionEvent.ACTION_UP) {
+                    Log.v(TAG, "removing callback...");
+                    v.removeCallbacks(r);
+                }
+                return true;
+            }
+        });
+        */
     }
 
     public void onClickVolumeMl(View v) {
@@ -65,7 +122,7 @@ public class CyclesActivity extends AppCompatActivity {
     private SeekBar.OnSeekBarChangeListener seekBarListener() {
         return new SeekBar.OnSeekBarChangeListener() {
             @Override
-            public void onProgressChanged(SeekBar bar, int idx, boolean unused) {
+            public void onProgressChanged(SeekBar unused, int idx, boolean unused1) {
                 int val = currentParmValues.get(idx);
                 currentParmButton.setText(Integer.toString(val));
             }
@@ -98,19 +155,9 @@ public class CyclesActivity extends AppCompatActivity {
     private void setSeekBar(int idx) {
         seekBar.setProgress(idx);
         int val = currentParmValues.get(idx);
-        seekBar.setProgress(idx);
         currentParmButton.setText(Integer.toString(val));
     }
 
-    /*
-    public void onClickDeleteCycle(View unused) {
-        Log.v(TAG, "child counr = " + parmTable.getChildCount());
-        for (int i = 0; i < parmTable.getChildCount(); i++) {
-            View v = parmTable.getChildAt(i);
-            Log.v(TAG, "child " + i + " is " + (v == null ? "null" : "non-null"));
-        }
-    }
-    */
     public void onClickAddCycle(View unused) {
         for (int i = 2; i < parmTable.getChildCount(); i++) {
             View v = parmTable.getChildAt(i);
