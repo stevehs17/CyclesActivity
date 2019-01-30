ebbfb6753600df05839721e480670d761f0c4699
Steve <stevehs17@gmail.com>
2019-01-30 13:11:56 -0800

    Added and validated more tests for Checker.

--- app/src/test/java/com/ssimon/cyclesactivity/CheckerTest.java
+++ app/src/test/java/com/ssimon/cyclesactivity/CheckerTest.java
@@ -72,4 +72,67 @@ public class CheckerTest {
         }
         throw new IllegalStateException("failure");
     }
+
+    @Test
+    public void greaterThan_double_isCorrect() throws Exception {
+        Checker.greaterThan(DOUBLEVAL, DOUBLEMIN);
+    }
+
+    @Test
+    public void greaterThan_double_isIncorrect() throws Exception {
+        try {
+            Checker.greaterThan(DOUBLEVAL, DOUBLEVAL);
+        } catch (IllegalStateException e) {
+            return;
+        }
+        throw new IllegalStateException("failure");
+    }
+
+    /*
+
+    @Test
+    public void atLeast_float_isCorrect() throws Exception {
+        Checker.atLeast(FLOATVAL, FLOATMIN);
+    }
+
+    @Test
+    public void atLeast_float_isIncorrect() throws Exception {
+        try {
+            Checker.atLeast(FLOATMIN, FLOATVAL);
+        } catch (IllegalStateException e) {
+            return;
+        }
+        throw new IllegalStateException("failure");
+    }
+
+    @Test
+    public void atLeast_int_isCorrect() throws Exception {
+        Checker.atLeast(INTVAL, INTMIN);
+    }
+
+    @Test
+    public void atLeast_int_isIncorrect() throws Exception {
+         try {
+            Checker.atLeast(INTMIN, INTVAL);
+        } catch (IllegalStateException e) {
+            return;
+        }
+        throw new IllegalStateException("failure");
+    }
+
+    @Test
+    public void atLeast_long_isCorrect() throws Exception {
+        Checker.atLeast(LONGVAL, LONGMIN);
+    }
+
+    @Test
+    public void atLeast_long_isIncorrect() throws Exception {
+        try {
+            Checker.atLeast(LONGMIN, LONGVAL);
+        } catch (IllegalStateException e) {
+            return;
+        }
+        throw new IllegalStateException("failure");
+    }
+     */
 }
