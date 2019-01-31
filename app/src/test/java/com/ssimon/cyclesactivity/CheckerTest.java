ee9c2e8a8389dfa3ff2bc6edfb913b790531b841
Steve <stevehs17@gmail.com>
2019-01-30 20:19:00 -0800

    Slighty improved naming of variables and methods in CheckerTest.java.

--- app/src/test/java/com/ssimon/cyclesactivity/CheckerTest.java
+++ app/src/test/java/com/ssimon/cyclesactivity/CheckerTest.java
@@ -9,15 +9,15 @@ import java.util.List;
 import java.util.Map;
 
 public class CheckerTest {
-    static final private double DOUBLEVAL = 5.1;
+    static final private double DOUBLEMAX = 5.1;
     static final private double DOUBLEMIN = 2.1;
-    static final private float FLOATVAL = 5.1F;
     static final private float FLOATMIN = 2.1F;
     static final private float FLOATMAX = 10.5F;
-    static final private int INTVAL = 5;
+    static final private float FLOATAVG = (FLOATMAX - FLOATMIN)/2;
     static final private int INTMIN = 2;
     static final private int INTMAX = 10;
-    static final private long LONGVAL = 5L;
+    static final private int INTAVG = (INTMAX - INTMIN)/2;
+    static final private long LONGMAX = 5L;
     static final private long LONGMIN = 2L;
     static final private List<String> EMPTY_COLLECTION = new ArrayList<>();
     static final private List<String> NONEMPTY_COLLECTION = Arrays.asList("test");
@@ -32,14 +32,14 @@ public class CheckerTest {
     static final private Object NULL_OBJECT = null;
 
     @Test
-    public void atLeast_double_isCorrect() throws Exception {
-        Checker.atLeast(DOUBLEVAL, DOUBLEMIN);
+    public void atLeast_double_Succeeds() throws Exception {
+        Checker.atLeast(DOUBLEMAX, DOUBLEMIN);
     }
 
     @Test
-    public void atLeast_double_isIncorrect() throws Exception {
+    public void atLeast_double_Fails() throws Exception {
         try {
-            Checker.atLeast(DOUBLEMIN, DOUBLEVAL);
+            Checker.atLeast(DOUBLEMIN, DOUBLEMAX);
         } catch (IllegalStateException e) {
             return;
         }
@@ -47,14 +47,14 @@ public class CheckerTest {
     }
 
     @Test
-    public void atLeast_float_isCorrect() throws Exception {
-        Checker.atLeast(FLOATVAL, FLOATMIN);
+    public void atLeast_float_Succeeds() throws Exception {
+        Checker.atLeast(FLOATAVG, FLOATMIN);
     }
 
     @Test
-    public void atLeast_float_isIncorrect() throws Exception {
+    public void atLeast_float_Fails() throws Exception {
         try {
-            Checker.atLeast(FLOATMIN, FLOATVAL);
+            Checker.atLeast(FLOATMIN, FLOATMAX);
         } catch (IllegalStateException e) {
             return;
         }
@@ -62,14 +62,14 @@ public class CheckerTest {
     }
 
     @Test
-    public void atLeast_int_isCorrect() throws Exception {
-        Checker.atLeast(INTVAL, INTMIN);
+    public void atLeast_int_Succeeds() throws Exception {
+        Checker.atLeast(INTMAX, INTMIN);
     }
 
     @Test
-    public void atLeast_int_isIncorrect() throws Exception {
+    public void atLeast_int_Fails() throws Exception {
          try {
-            Checker.atLeast(INTMIN, INTVAL);
+            Checker.atLeast(INTMIN, INTMAX);
         } catch (IllegalStateException e) {
             return;
         }
@@ -77,14 +77,14 @@ public class CheckerTest {
     }
 
     @Test
-    public void atLeast_long_isCorrect() throws Exception {
-        Checker.atLeast(LONGVAL, LONGMIN);
+    public void atLeast_long_Succeeds() throws Exception {
+        Checker.atLeast(LONGMAX, LONGMIN);
     }
 
     @Test
-    public void atLeast_long_isIncorrect() throws Exception {
+    public void atLeast_long_Fails() throws Exception {
         try {
-            Checker.atLeast(LONGMIN, LONGVAL);
+            Checker.atLeast(LONGMIN, LONGMAX);
         } catch (IllegalStateException e) {
             return;
         }
@@ -92,14 +92,14 @@ public class CheckerTest {
     }
 
     @Test
-    public void greaterThan_double_isCorrect() throws Exception {
-        Checker.greaterThan(DOUBLEVAL, DOUBLEMIN);
+    public void greaterThan_double_Succeeds() throws Exception {
+        Checker.greaterThan(DOUBLEMAX, DOUBLEMIN);
     }
 
     @Test
-    public void greaterThan_double_isIncorrect() throws Exception {
+    public void greaterThan_double_Fails() throws Exception {
         try {
-            Checker.greaterThan(DOUBLEVAL, DOUBLEVAL);
+            Checker.greaterThan(DOUBLEMAX, DOUBLEMAX);
         } catch (IllegalStateException e) {
             return;
         }
@@ -107,14 +107,14 @@ public class CheckerTest {
     }
 
     @Test
-    public void greaterThan_float_isCorrect() throws Exception {
-        Checker.greaterThan(FLOATVAL, FLOATMIN);
+    public void greaterThan_float_Succeeds() throws Exception {
+        Checker.greaterThan(FLOATMAX, FLOATMIN);
     }
 
     @Test
-    public void greaterThan_float_isIncorrect() throws Exception {
+    public void greaterThan_float_Fails() throws Exception {
         try {
-            Checker.greaterThan(FLOATVAL, FLOATVAL);
+            Checker.greaterThan(FLOATMIN, FLOATMIN);
         } catch (IllegalStateException e) {
             return;
         }
@@ -122,29 +122,14 @@ public class CheckerTest {
     }
 
     @Test
-    public void greaterThan_int_isCorrect() throws Exception {
-        Checker.greaterThan(INTVAL, INTMIN);
+    public void greaterThan_int_Succeeds() throws Exception {
+        Checker.greaterThan(INTMAX, INTMIN);
     }
 
     @Test
-    public void greaterThan_int_isIncorrect() throws Exception {
-         try {
-            Checker.greaterThan(INTVAL, INTVAL);
-        } catch (IllegalStateException e) {
-            return;
-        }
-        throw new IllegalStateException("failure");
-    }
-
-    @Test
-    public void greaterThan_long_isCorrect() throws Exception {
-        Checker.greaterThan(LONGVAL, LONGMIN);
-    }
-
-    @Test
-    public void greaterThan_long_isIncorrect() throws Exception {
+    public void greaterThan_int_Fails() throws Exception {
         try {
-            Checker.greaterThan(LONGVAL, LONGVAL);
+            Checker.greaterThan(INTMIN, INTMAX);
         } catch (IllegalStateException e) {
             return;
         }
@@ -152,14 +137,14 @@ public class CheckerTest {
     }
 
     @Test
-    public void inRange_float_isCorrect() throws Exception {
-        Checker.inRange(FLOATVAL, FLOATMIN, FLOATMAX);
+    public void greaterThan_long_Succeeds() throws Exception {
+        Checker.greaterThan(LONGMAX, LONGMIN);
     }
 
     @Test
-    public void inRange_float_isIncorrect() throws Exception {
+    public void greaterThan_long_Fails() throws Exception {
         try {
-            Checker.inRange(FLOATMIN, FLOATVAL, FLOATMAX);
+            Checker.greaterThan(LONGMAX, LONGMAX);
         } catch (IllegalStateException e) {
             return;
         }
@@ -167,14 +152,14 @@ public class CheckerTest {
     }
 
     @Test
-    public void inRange_int_isCorrect() throws Exception {
-        Checker.inRange(INTVAL, INTMIN, INTMAX);
+    public void inRange_float_Succeeds() throws Exception {
+        Checker.inRange(FLOATAVG, FLOATMIN, FLOATMAX);
     }
 
     @Test
-    public void inRange_int_isIncorrect() throws Exception {
+    public void inRange_float_Fails() throws Exception {
         try {
-            Checker.inRange(INTMIN, INTVAL, INTMAX);
+            Checker.inRange(FLOATMIN, FLOATAVG, FLOATMAX);
         } catch (IllegalStateException e) {
             return;
         }
@@ -182,14 +167,14 @@ public class CheckerTest {
     }
 
     @Test
-    public void isExactly_int_isCorrect() throws Exception {
-        Checker.isExactly(INTVAL, INTVAL);
+    public void inRange_int_Succeeds() throws Exception {
+        Checker.inRange(INTAVG, INTMIN, INTMAX);
     }
 
     @Test
-    public void isExactly_int_isIncorrect() throws Exception {
+    public void inRange_int_Fails() throws Exception {
         try {
-            Checker.isExactly(INTMIN, INTVAL);
+            Checker.inRange(INTMIN, INTAVG, INTMAX);
         } catch (IllegalStateException e) {
             return;
         }
@@ -197,14 +182,14 @@ public class CheckerTest {
     }
 
     @Test
-    public void lessThan_int_isCorrect() throws Exception {
-        Checker.lessThan(INTMIN, INTVAL);
+    public void isExactly_int_Succeeds() throws Exception {
+        Checker.isExactly(INTAVG, INTAVG);
     }
 
     @Test
-    public void lessThan_int_isIncorrect() throws Exception {
+    public void isExactly_int_Fails() throws Exception {
         try {
-            Checker.lessThan(INTVAL, INTVAL);
+            Checker.isExactly(INTMIN, INTAVG);
         } catch (IllegalStateException e) {
             return;
         }
@@ -212,12 +197,27 @@ public class CheckerTest {
     }
 
     @Test
-    public void notEmpty_Collection_isCorrect() throws Exception {
+    public void lessThan_int_Succeeds() throws Exception {
+        Checker.lessThan(INTMIN, INTAVG);
+    }
+
+    @Test
+    public void lessThan_int_Fails() throws Exception {
+        try {
+            Checker.lessThan(INTAVG, INTAVG);
+        } catch (IllegalStateException e) {
+            return;
+        }
+        throw new IllegalStateException("failure");
+    }
+
+    @Test
+    public void notEmpty_Collection_Succeeds() throws Exception {
         Checker.notEmpty(NONEMPTY_COLLECTION);
     }
 
     @Test
-    public void notEmpty_Collection_isIncorrect() throws Exception {
+    public void notEmpty_Collection_Fails() throws Exception {
         try {
             Checker.notEmpty(EMPTY_COLLECTION);
         } catch (IllegalStateException e) {
@@ -227,12 +227,12 @@ public class CheckerTest {
     }
 
     @Test
-    public void notEmpty_Map_isCorrect() throws Exception {
+    public void notEmpty_Map_Succeeds() throws Exception {
         Checker.notEmpty(NONEMPTY_MAP);
     }
 
     @Test
-    public void notEmpty_Map_isIncorrect() throws Exception {
+    public void notEmpty_Map_Fails() throws Exception {
         try {
             Checker.notEmpty(EMPTY_MAP);
         } catch (IllegalStateException e) {
@@ -242,12 +242,12 @@ public class CheckerTest {
     }
 
     @Test
-    public void notEmpty_String_isCorrect() throws Exception {
+    public void notEmpty_String_Succeeds() throws Exception {
         Checker.notEmpty(NONEMPTY_STRING);
     }
 
     @Test
-    public void notEmpty_String_isIncorrect() throws Exception {
+    public void notEmpty_String_Fails() throws Exception {
         try {
             Checker.notEmpty(EMPTY_STRING);
         } catch (IllegalStateException e) {
@@ -257,12 +257,12 @@ public class CheckerTest {
     }
 
     @Test
-    public void notEquals_int_isCorrect() throws Exception {
+    public void notEquals_int_Succeeds() throws Exception {
         Checker.notEquals(INTMIN, INTMAX);
     }
 
     @Test
-    public void notEquals_String_isIncorrect() throws Exception {
+    public void notEquals_String_Fails() throws Exception {
         try {
             Checker.notEquals(INTMIN, INTMIN);
         } catch (IllegalStateException e) {
@@ -272,12 +272,12 @@ public class CheckerTest {
     }
 
     @Test
-    public void notGreaterThan_float_isCorrect() throws Exception {
+    public void notGreaterThan_float_Succeeds() throws Exception {
         Checker.notGreaterThan(FLOATMIN, FLOATMIN);
     }
 
     @Test
-    public void notGreaterThan_float_isIncorrect() throws Exception {
+    public void notGreaterThan_float_Fails() throws Exception {
         try {
             Checker.notGreaterThan(FLOATMAX, FLOATMIN);
         } catch (IllegalStateException e) {
@@ -287,12 +287,12 @@ public class CheckerTest {
     }
 
     @Test
-    public void notGreaterThan_int_isCorrect() throws Exception {
+    public void notGreaterThan_int_Succeeds() throws Exception {
         Checker.notGreaterThan(INTMIN, INTMIN);
     }
 
     @Test
-    public void notGreaterThan_int_isIncorrect() throws Exception {
+    public void notGreaterThan_int_Fails() throws Exception {
         try {
             Checker.notGreaterThan(INTMAX, INTMIN);
         } catch (IllegalStateException e) {
@@ -302,12 +302,12 @@ public class CheckerTest {
     }
 
     @Test
-    public void notNull_Collection_isCorrect() throws Exception {
+    public void notNull_Collection_Succeeds() throws Exception {
         Checker.notNull(EMPTY_COLLECTION);
     }
 
     @Test
-    public void notNull_Collection_isIncorrect() throws Exception {
+    public void notNull_Collection_Fails() throws Exception {
         try {
             Checker.notNull(NULL_COLLECTION);
         } catch (NullPointerException e) {
@@ -317,12 +317,12 @@ public class CheckerTest {
     }
 
     @Test
-    public void notNull_Map_isCorrect() throws Exception {
+    public void notNull_Map_Succeeds() throws Exception {
         Checker.notNull(EMPTY_MAP);
     }
 
     @Test
-    public void notNull_Map_isIncorrect() throws Exception {
+    public void notNull_Map_Fails() throws Exception {
         try {
             Checker.notNull(NULL_MAP);
         } catch (NullPointerException e) {
@@ -332,12 +332,12 @@ public class CheckerTest {
     }
 
     @Test
-    public void notNull_Object_isCorrect() throws Exception {
+    public void notNull_Object_Succeeds() throws Exception {
         Checker.notNull(NONNULL_OBJECT);
     }
 
     @Test
-    public void notNull_Object_isIncorrect() throws Exception {
+    public void notNull_Object_Fails() throws Exception {
         try {
             Checker.notNull(NULL_OBJECT);
         } catch (NullPointerException e) {
@@ -347,12 +347,12 @@ public class CheckerTest {
     }
 
     @Test
-    public void notNull_String_isCorrect() throws Exception {
+    public void notNull_String_Succeeds() throws Exception {
         Checker.notNull(EMPTY_STRING);
     }
 
     @Test
-    public void notNull_String_isIncorrect() throws Exception {
+    public void notNull_String_Fails() throws Exception {
         try {
             Checker.notNull(NULL_STRING);
         } catch (NullPointerException e) {
@@ -362,12 +362,12 @@ public class CheckerTest {
     }
 
     @Test
-    public void notNullOrEmpty_Collection_isCorrect() throws Exception {
+    public void notNullOrEmpty_Collection_Succeeds() throws Exception {
         Checker.notNullOrEmpty(NONEMPTY_COLLECTION);
     }
 
     @Test
-    public void notNullOrEmpty_Collection_null_isIncorrect() throws Exception {
+    public void notNullOrEmpty_Collection_null_Fails() throws Exception {
         try {
             Checker.notNullOrEmpty(NULL_COLLECTION);
         } catch (NullPointerException e) {
@@ -377,7 +377,7 @@ public class CheckerTest {
     }
 
     @Test
-    public void notNullOrEmptyCollection_empty_isIncorrect() throws Exception {
+    public void notNullOrEmptyCollection_empty_Fails() throws Exception {
         try {
             Checker.notNullOrEmpty(EMPTY_COLLECTION);
         } catch (IllegalStateException e) {
@@ -387,12 +387,12 @@ public class CheckerTest {
     }
 
     @Test
-    public void notNullOrEmpty_Map_isCorrect() throws Exception {
+    public void notNullOrEmpty_Map_Succeeds() throws Exception {
         Checker.notNullOrEmpty(NONEMPTY_MAP);
     }
 
     @Test
-    public void notNullOrEmpty_Map_null_isIncorrect() throws Exception {
+    public void notNullOrEmpty_Map_null_Fails() throws Exception {
         try {
             Checker.notNullOrEmpty(NULL_MAP);
         } catch (NullPointerException e) {
@@ -402,7 +402,7 @@ public class CheckerTest {
     }
 
     @Test
-    public void notNullOrEmpty_Map_empty_isIncorrect() throws Exception {
+    public void notNullOrEmpty_Map_empty_Fails() throws Exception {
         try {
             Checker.notNullOrEmpty(EMPTY_MAP);
         } catch (IllegalStateException e) {
@@ -412,12 +412,12 @@ public class CheckerTest {
     }
 
     @Test
-    public void notNullOrEmpty_String_isCorrect() throws Exception {
+    public void notNullOrEmpty_String_Succeeds() throws Exception {
         Checker.notNullOrEmpty(NONEMPTY_STRING);
     }
 
     @Test
-    public void notNullOrEmpty_String_null_isIncorrect() throws Exception {
+    public void notNullOrEmpty_String_null_Fails() throws Exception {
         try {
             Checker.notNullOrEmpty(NULL_STRING);
         } catch (NullPointerException e) {
@@ -427,7 +427,7 @@ public class CheckerTest {
     }
 
     @Test
-    public void notNullOrEmpty_String_empty_isIncorrect() throws Exception {
+    public void notNullOrEmpty_String_empty_Fails() throws Exception {
         try {
             Checker.notNullOrEmpty(EMPTY_STRING);
         } catch (IllegalStateException e) {
@@ -437,12 +437,12 @@ public class CheckerTest {
     }
 
     @Test
-    public void notNullResourceId_isCorrect() throws Exception {
+    public void notNullResourceId_Succeeds() throws Exception {
         Checker.notNullResourceId(Const.NULL_RESOURCE_ID + 1);
     }
 
     @Test
-    public void notNullResourceId_isIncorrect() throws Exception {
+    public void notNullResourceId_Fails() throws Exception {
         try {
             Checker.notNullResourceId(Const.NULL_RESOURCE_ID);
         } catch (IllegalStateException e) {
