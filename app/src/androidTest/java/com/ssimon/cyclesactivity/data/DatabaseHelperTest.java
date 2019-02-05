4588624d504ca7c5e0cc3861e605b7b522ab01f9
Steve <stevehs17@gmail.com>
2019-02-05 15:16:16 -0800

    In progress: debugging test for db.

---
+++ app/src/androidTest/java/com/ssimon/cyclesactivity/data/DatabaseHelperTest.java
@@ -0,0 +1,32 @@
+package com.ssimon.cyclesactivity.data;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.support.test.InstrumentationRegistry;
+
+import com.ssimon.cyclesactivity.data.DatabaseHelper;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+
+public class DatabaseHelperTest {
+    final private Context context = InstrumentationRegistry.getTargetContext();
+
+    @Test
+    public void testDb() {
+        DatabaseHelper dh = DatabaseHelper.getInstance(context);
+        SQLiteDatabase db = dh.getWritableDatabase();
+        db.execSQL(Contract.Recipe.DELETE_TABLE);
+
+        //db.execSQL(Contract.Volume.DELETE_TABLE);
+        //db.execSQL(Contract.Cycle.DELETE_TA/BLE);
+
+        db.execSQL(Contract.Recipe.CREATE_TABLE);
+
+        //db.execSQL(Contract.Volume.CREATE_TABLE);
+        //db.execSQL(Contract.Cycle.CREATE_TABLE);
+    }
+}
---
+++ app/src/main/java/com/ssimon/cyclesactivity/data/DatabaseHelper.java
@@ -0,0 +1,45 @@
+package com.ssimon.cyclesactivity.data;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import com.ssimon.cyclesactivity.util.Checker;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+    static final private int DATABASE_VERSION = 1;
+    static final private String DATABASE_NAME = "groundcontrol_database.db";
+    static private DatabaseHelper singleton = null;
+
+    static synchronized DatabaseHelper getInstance(Context c) {
+        Checker.notNull(c);
+        if (singleton == null)
+            singleton = new DatabaseHelper(c.getApplicationContext());
+        return singleton;
+    }
+
+    private DatabaseHelper(Context c) {
+        super(c, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(Contract.Recipe.CREATE_TABLE);
+        db.execSQL(Contract.Volume.CREATE_TABLE);
+        //db.execSQL(Contract.Cycle.CREATE_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL(Contract.Recipe.CREATE_TABLE);
+        db.execSQL(Contract.Volume.CREATE_TABLE);
+        db.execSQL(Contract.Cycle.CREATE_TABLE);
+        onConfigure(db);
+    }
+
+    @Override
+    public void onConfigure(SQLiteDatabase db) {
+        super.onConfigure(db);
+        db.setForeignKeyConstraintsEnabled(true);
+    }
+}
