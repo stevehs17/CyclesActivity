0f67d9b04dc3010288b7bedc687458912d84a536
Steve <stevehs17@gmail.com>
2019-02-06 15:36:40 -0800

    Implemented and tested test that validates values of Coffee object saved to and retrieved from database.

--- app/src/androidTest/java/com/ssimon/cyclesactivity/data/CoffeeDaoTest.java
+++ app/src/androidTest/java/com/ssimon/cyclesactivity/data/CoffeeDaoTest.java
@@ -2,14 +2,22 @@ package com.ssimon.cyclesactivity.data;
 
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
+import android.security.keystore.UserNotAuthenticatedException;
 import android.support.test.InstrumentationRegistry;
 
+import com.ssimon.cyclesactivity.Const;
 import com.ssimon.cyclesactivity.DatabaseUtils;
 import com.ssimon.cyclesactivity.ModelUtils;
 import com.ssimon.cyclesactivity.model.Coffee;
+import com.ssimon.cyclesactivity.model.Cycle;
+import com.ssimon.cyclesactivity.model.Volume;
 
 import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 public class CoffeeDaoTest {
@@ -29,5 +37,44 @@ public class CoffeeDaoTest {
         insertCoffee_Success();
         SQLiteDatabase db = DatabaseUtils.getReadableleDb(context);
         List<Coffee> cs = CoffeeDao.getCoffees(db);
+    }
+
+    @Test
+    public void getCoffees_validate_values_Success() {
+        final int volumeMl = (Cycle.MAX_VOLUME + Cycle.MIN_BREWTIME)/2;
+        final int brewTime = (Cycle.MIN_BREWTIME + Cycle.MAX_BREWTIME)/2;
+        final int vacuumTime = (Cycle.MIN_VACUUMTIME+ Cycle.MAX_VACUUMTIME)/2;
+        final String name = "aname";
+
+        Cycle cyc = new Cycle(Const.UNSET_DATABASE_ID, volumeMl, brewTime, vacuumTime);
+        List<Cycle> cycles = new ArrayList<>();
+        cycles.add(cyc);
+        Volume vol = new Volume(Const.UNSET_DATABASE_ID, cycles);
+        List<Volume> vols = new ArrayList<>();
+        vols.add(vol);
+        Coffee cof = new Coffee(Const.UNSET_DATABASE_ID, name, vols);
+
+        DatabaseHelperTest dht = new DatabaseHelperTest();
+        dht.reset_tables_and_open_db_Success();
+        SQLiteDatabase db = DatabaseUtils.getWritableDb(context);
+        CoffeeDao.insertCoffee(db, cof);
+
+        db = DatabaseUtils.getReadableleDb(context);
+        List<Coffee> cs = CoffeeDao.getCoffees(db);
+        Coffee cofOut = cs.get(0);
+        assertNotEquals(cof.id(), cofOut.id());
+        assertEquals(1, cofOut.id());
+        assertEquals(cof.name(), cofOut.name());
+
+        Volume volOut = cofOut.volumes().get(0);
+        assertNotEquals(vol.id(), volOut.id());
+        assertEquals(1, volOut.id());
+
+        Cycle cycOut = volOut.cycles().get(0);
+        assertNotEquals(cyc.volumeId(), cycOut.volumeId());
+        assertEquals(1, cycOut.volumeId());
+        assertEquals(cyc.volumeMl(), cycOut.volumeMl());
+        assertEquals(cyc.brewSeconds(), cycOut.brewSeconds());
+        assertEquals(cyc.vacuumSeconds(), cycOut.vacuumSeconds());
     }
 }
