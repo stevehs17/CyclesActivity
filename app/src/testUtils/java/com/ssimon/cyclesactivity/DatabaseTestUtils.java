4aae73fda3293d846a517b5bd00b931e97dd2fca
Steve <stevehs17@gmail.com>
2019-02-13 13:03:38 -0800

    Cleaned up. All tests have just been run and worked.

--- app/src/testUtils/java/com/ssimon/cyclesactivity/DatabaseTestUtils.java
+++ app/src/testUtils/java/com/ssimon/cyclesactivity/DatabaseTestUtils.java
@@ -7,26 +7,19 @@ import com.ssimon.cyclesactivity.data.DatabaseHelper;
 
 public class DatabaseTestUtils {
     static public SQLiteDatabase getWritableDb(Context ctx) {
-        //DatabaseHelper dh = getDatabaseHelper(ctx);
         DatabaseHelper dh = DatabaseHelper.getInstance(ctx);
         return dh.getWritableDatabase();
     }
 
     static public SQLiteDatabase getReadableleDb(Context ctx) {
-        //DatabaseHelper dh = getDatabaseHelper(ctx);
         DatabaseHelper dh = DatabaseHelper.getInstance(ctx);
         return dh.getReadableDatabase();
     }
 
     static public SQLiteDatabase getCleanWritableDb(Context ctx) {
-        //DatabaseHelper dh = getDatabaseHelper(ctx);
         DatabaseHelper dh = DatabaseHelper.getInstance(ctx);
         SQLiteDatabase db = dh.getWritableDatabase();
         dh.onUpgrade(db, 0, 0);
         return db;
-    }
-
-    static private DatabaseHelper getDatabaseHelper(Context ctx) {
-        return DatabaseHelper.getInstance(ctx);
     }
 }
