2cf92affd1cabf52ebe5d699d7ca0688d52e6b64
Steve <stevehs17@gmail.com>
2019-04-07 18:10:32 -0700

    In progress: adding link Canary.
    All the relevant changes should have been made.
    But haven't found any memory leaks so far.
    Will next force a leak to validate and debug installation.

---
+++ app/src/main/java/com/ssimon/cyclesactivity/CanaryApplication.java
@@ -0,0 +1,27 @@
+package com.ssimon.cyclesactivity;
+
+import android.app.Application;
+import android.content.Context;
+
+import com.squareup.leakcanary.LeakCanary;
+import com.squareup.leakcanary.RefWatcher;
+import com.ssimon.cyclesactivity.util.Checker;
+
+public class CanaryApplication extends Application {
+    public static CanaryApplication instance;
+    private RefWatcher refWatcher;
+
+    static public RefWatcher getRefWatcher(Context c) {
+        Checker.notNull(c);
+        CanaryApplication app = (CanaryApplication) c.getApplicationContext();
+        return app.refWatcher;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        if (LeakCanary.isInAnalyzerProcess(this))
+            return; // process is dedicated to LeakCanary for heap analysis.
+        refWatcher = LeakCanary.install(this);
+    }
+}
