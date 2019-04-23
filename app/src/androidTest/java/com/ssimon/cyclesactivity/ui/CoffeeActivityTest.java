958ff0d30fed6d6c2085676ce8fcb2bccd47ee75
Steve <stevehs17@gmail.com>
2019-04-23 13:24:50 -0700

    Fixed UI test after making changes to CoffeeActivity.

--- app/src/androidTest/java/com/ssimon/cyclesactivity/ui/CoffeeActivityTest.java
+++ app/src/androidTest/java/com/ssimon/cyclesactivity/ui/CoffeeActivityTest.java
@@ -1,28 +1,16 @@
 package com.ssimon.cyclesactivity.ui;
 
 
-import android.content.Context;
-import android.database.sqlite.SQLiteDatabase;
-import android.support.test.InstrumentationRegistry;
 import android.support.test.espresso.ViewInteraction;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.test.suitebuilder.annotation.LargeTest;
 
 import com.ssimon.cyclesactivity.R;
-import com.ssimon.cyclesactivity.data.CoffeeDao;
-import com.ssimon.cyclesactivity.data.DatabaseHelper;
-import com.ssimon.cyclesactivity.data.DatabaseTestUtils;
-import com.ssimon.cyclesactivity.model.Coffee;
-import com.ssimon.cyclesactivity.util.ModelUtils;
 
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
-import java.util.List;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
@@ -35,68 +23,42 @@ import static org.hamcrest.Matchers.allOf;
 
 @LargeTest
 @RunWith(AndroidJUnit4.class)
-public class EditActivitiesNavigateTest {
+public class CoffeeActivityTest {
 
     @Rule
-    public ActivityTestRule<CoffeeActivity> activityRule = new ActivityTestRule<>
-            (CoffeeActivity.class, false, false);
-
-    @BeforeClass
-    static public void setupDatabase() {
-        DatabaseTestUtils.setupDatabase();
-    }
-
-    @Before
-    public void setupTables() {
-        DatabaseTestUtils.setupTables();
-
-        Context ctx = InstrumentationRegistry.getTargetContext();
-        DatabaseHelper dh = DatabaseHelper.getInstance(ctx);
-        SQLiteDatabase db = dh.getWritableDatabase();
-        List<Coffee> cs = ModelUtils.createDefaultCoffeeTemplates();
-        CoffeeDao.insertCoffees(db, cs);
-        activityRule.launchActivity(null);
-    }
+    public ActivityTestRule<CoffeeActivity> mActivityTestRule = new ActivityTestRule<>(CoffeeActivity.class);
 
     @Test
-    public void navigateEditActivities_Success() {
-        descendToVolumes();
-        descendToCycles();
-        ascendToVolumes();
-        ascendToCoffees();
-    }
-
-    private void descendToVolumes() {
+    public void coffeeActivityTest() {
         ViewInteraction appCompatButton = onView(
                 allOf(withText("Edit Volumes"), isDisplayed()));
         appCompatButton.perform(click());
 
-    }
+        ViewInteraction appCompatTextView = onView(
+                allOf(withId(android.R.id.text1), withText("1200 ml"), isDisplayed()));
+        appCompatTextView.perform(click());
 
-    private void descendToCycles() {
         ViewInteraction appCompatButton2 = onView(
                 allOf(withText("Edit Cycles"),
                         withParent(allOf(withId(R.id.activity_managecoffees),
                                 withParent(withId(android.R.id.content)))),
                         isDisplayed()));
         appCompatButton2.perform(click());
-    }
 
-    private void ascendToVolumes() {
         ViewInteraction appCompatImageButton = onView(
                 allOf(withContentDescription("Navigate up"),
                         withParent(allOf(withId(R.id.action_bar),
                                 withParent(withId(R.id.action_bar_container)))),
                         isDisplayed()));
         appCompatImageButton.perform(click());
-    }
 
-    private void ascendToCoffees() {
-        ViewInteraction appCompatImageButton = onView(
+        ViewInteraction appCompatImageButton2 = onView(
                 allOf(withContentDescription("Navigate up"),
                         withParent(allOf(withId(R.id.action_bar),
                                 withParent(withId(R.id.action_bar_container)))),
                         isDisplayed()));
-        appCompatImageButton.perform(click());
+        appCompatImageButton2.perform(click());
+
     }
+
 }
