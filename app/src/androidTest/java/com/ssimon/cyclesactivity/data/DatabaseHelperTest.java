1d75f4176c7e541a5cc4880522efe5a3f2689f03
Steve <stevehs17@gmail.com>
2019-05-06 17:32:33 -0700

    In progress: adding DatabaseHelper tests.

--- app/src/androidTest/java/com/ssimon/cyclesactivity/data/DatabaseHelperTest.java
+++ app/src/androidTest/java/com/ssimon/cyclesactivity/data/DatabaseHelperTest.java
@@ -4,14 +4,20 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.test.InstrumentationRegistry;
 
+import com.ssimon.cyclesactivity.model.Coffee;
+import com.ssimon.cyclesactivity.model.Cycle;
+import com.ssimon.cyclesactivity.model.Volume;
+
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import java.util.List;
+
 import static org.junit.Assert.assertTrue;
 
 public class DatabaseHelperTest {
-    private DatabaseHelper dbHelper;
+    private DatabaseHelper dbHelp;
 
     @BeforeClass
     static public void setupDatabase() {
@@ -21,18 +27,63 @@ public class DatabaseHelperTest {
     @Before
     public void resetDb() {
         final Context c = InstrumentationRegistry.getTargetContext();
-        dbHelper = dbHelper.getInstance(c);
+        dbHelp = dbHelp.getInstance(c);
     }
 
     @Test
     public void openWritableDb_Succeeds() {
-        SQLiteDatabase db = dbHelper.getWritableDatabase();
+        SQLiteDatabase db = dbHelp.getWritableDatabase();
         assertTrue(db.isOpen());
     }
 
     @Test
     public void openReadableDb_Succeeds() {
-        SQLiteDatabase db = dbHelper.getReadableDatabase();
+        SQLiteDatabase db = dbHelp.getReadableDatabase();
         assertTrue(db.isOpen());
     }
+
+    @Test
+    public void deleteCoffee_Succeeds() {
+        List<Coffee> cs = DatabaseTestUtils.createCoffees(1, 1);
+        SQLiteDatabase db = dbHelp.getWritableDatabase();
+        CoffeeDao.insertCoffees(db, cs);
+        List<Coffee> csOut = CoffeeDao.getCoffees(db);
+        dbHelp.deleteCoffee(csOut.get(0).id());
+    }
+
+    @Test
+    public void deleteVolume_Succeeds() {
+        List<Coffee> cs = DatabaseTestUtils.createCoffees(1, 1);
+        SQLiteDatabase db = dbHelp.getWritableDatabase();
+        CoffeeDao.insertCoffees(db, cs);
+        List<Coffee> csOut = CoffeeDao.getCoffees(db);
+        long id = csOut.get(0).volumes().get(0).id();
+        dbHelp.deleteVolume(id);
+    }
+
+    @Test
+    public void refreshCoffeeCache_Succeeds() {
+        List<Coffee> cs = DatabaseTestUtils.createCoffees(1, 1);
+        SQLiteDatabase db = dbHelp.getWritableDatabase();
+        CoffeeDao.insertCoffees(db, cs);
+        dbHelp.refreshCoffeeCache();
+    }
+
+    @Test
+    public void saveVolume_Succeeds() {
+        List<Coffee> cs = DatabaseTestUtils.createCoffees(1, 1);
+        SQLiteDatabase db = dbHelp.getWritableDatabase();
+        CoffeeDao.insertCoffees(db, cs);
+        List<Coffee> csOut = CoffeeDao.getCoffees(db);
+        List<Cycle> cycles = DatabaseTestUtils.createCycles(Volume.MAX_NUM_CYCLES);
+        dbHelp.saveVolume(csOut.get(0).id(), cycles);
+    }
+
+    @Test
+    public void saveCoffee_Succeeds() {
+        List<Coffee> cs = DatabaseTestUtils.createCoffees(1, 1);
+        dbHelp.saveCoffee(cs.get(0));
+    }
+
+
 }
--- app/src/main/java/com/ssimon/cyclesactivity/data/DatabaseHelper.java
+++ app/src/main/java/com/ssimon/cyclesactivity/data/DatabaseHelper.java
@@ -7,10 +7,12 @@ import android.os.Process;
 
 import com.ssimon.cyclesactivity.Const;
 import com.ssimon.cyclesactivity.model.Coffee;
+import com.ssimon.cyclesactivity.model.Volume;
 import com.ssimon.cyclesactivity.util.ModelUtils;
 import com.ssimon.cyclesactivity.model.Cycle;
 import com.ssimon.cyclesactivity.util.Checker;
 
+import java.util.Collections;
 import java.util.List;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
@@ -48,71 +50,6 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     public void onConfigure(SQLiteDatabase db) {
         super.onConfigure(db);
         db.setForeignKeyConstraintsEnabled(true);
-    }
-
-    public void refreshCoffeeCache() {
-        new RefreshCoffeeCacheThread().start();
-    }
-
-    private class RefreshCoffeeCacheThread extends BackgroundThread {
-        RefreshCoffeeCacheThread() {
-            super();
-        }
-
-        @Override
-        public void run() {
-            super.run();
-            SQLiteDatabase db = getReadableDatabase();
-            List<Coffee> cs = CoffeeDao.getCoffees(db);
-            CoffeeCache.setCoffees(cs);
-        }
-    }
-
-    public void saveCoffee(Coffee c) {
-        new SaveCoffeeThread(c).start();
-    }
-
-    private class SaveCoffeeThread extends BackgroundThread {
-        final private Coffee coffee;
-
-        SaveCoffeeThread(Coffee coffee) {
-            super();
-            Checker.notNull(coffee);
-            this.coffee = coffee;
-        }
-
-        @Override
-        public void run() {
-            super.run();
-            SQLiteDatabase db = getWritableDatabase();
-            CoffeeDao.insertCoffee(db, coffee);
-            refreshCoffeeCache();
-        }
-    }
-
-    public void saveVolume(long coffeeId, List<Cycle> cycles) {
-        new SaveVolumeThread(coffeeId, cycles).start();
-    }
-
-    private class SaveVolumeThread extends BackgroundThread {
-        final private long coffeeId;
-        final private List<Cycle> cycles;
-
-        SaveVolumeThread(long coffeeId, List<Cycle> cycles) {
-            super();
-            Checker.atLeast(coffeeId, Const.MIN_DATABASE_ID);
-            Checker.notNullOrEmpty(cycles);
-            this.coffeeId = coffeeId;
-            this.cycles = cycles;
-        }
-
-        @Override
-        public void run() {
-            super.run();
-            SQLiteDatabase db = getWritableDatabase();
-            VolumeDao.insertVolume(db, coffeeId, cycles);
-            refreshCoffeeCache();
-        }
     }
 
     public void deleteCoffee(long coffeeId) {
@@ -158,6 +95,98 @@ public class DatabaseHelper extends SQLiteOpenHelper {
             refreshCoffeeCache();
         }
     }
+
+    public void refreshCoffeeCache() {
+        new RefreshCoffeeCacheThread().start();
+    }
+
+    private class RefreshCoffeeCacheThread extends BackgroundThread {
+        RefreshCoffeeCacheThread() {
+            super();
+        }
+
+        @Override
+        public void run() {
+            super.run();
+            SQLiteDatabase db = getReadableDatabase();
+            List<Coffee> cs = CoffeeDao.getCoffees(db);
+            CoffeeCache.setCoffees(cs);
+        }
+    }
+
+    public void replaceVolume(long volumeId, List<Cycle> cycles) {
+        new ReplaceVolumeThread(volumeId, cycles).start();
+    }
+
+    private class ReplaceVolumeThread extends BackgroundThread {
+        final private long volumeId;
+        final private List<Cycle> cycles;
+
+        ReplaceVolumeThread(long volumeId, List<Cycle> cycles) {
+            super();
+            Checker.atLeast(volumeId, Const.MIN_DATABASE_ID);
+            Checker.notNull(cycles);
+            Checker.inRange(cycles.size(), Volume.MIN_NUM_CYCLES, Volume.MAX_NUM_CYCLES);
+            this.volumeId = volumeId;
+            this.cycles = Collections.unmodifiableList(cycles);
+        }
+
+        @Override
+        public void run() {
+            super.run();
+            SQLiteDatabase db = getWritableDatabase();
+            CycleDao.replaceCycles(db, volumeId, cycles);
+            refreshCoffeeCache();
+        }
+    }
+
+    public void saveCoffee(Coffee c) {
+        new SaveCoffeeThread(c).start();
+    }
+
+    private class SaveCoffeeThread extends BackgroundThread {
+        final private Coffee coffee;
+
+        SaveCoffeeThread(Coffee coffee) {
+            super();
+            Checker.notNull(coffee);
+            this.coffee = coffee;
+        }
+
+        @Override
+        public void run() {
+            super.run();
+            SQLiteDatabase db = getWritableDatabase();
+            CoffeeDao.insertCoffee(db, coffee);
+            refreshCoffeeCache();
+        }
+    }
+
+    public void saveVolume(long coffeeId, List<Cycle> cycles) {
+        new SaveVolumeThread(coffeeId, cycles).start();
+    }
+
+    private class SaveVolumeThread extends BackgroundThread {
+        final private long coffeeId;
+        final private List<Cycle> cycles;
+
+        SaveVolumeThread(long coffeeId, List<Cycle> cycles) {
+            super();
+            Checker.atLeast(coffeeId, Const.MIN_DATABASE_ID);
+            Checker.notNullOrEmpty(cycles);
+            this.coffeeId = coffeeId;
+            this.cycles = Collections.unmodifiableList(cycles);
+        }
+
+        @Override
+        public void run() {
+            super.run();
+            SQLiteDatabase db = getWritableDatabase();
+            VolumeDao.insertVolume(db, coffeeId, cycles);
+            refreshCoffeeCache();
+        }
+    }
+
     abstract private class BackgroundThread extends Thread {
         @Override
         public void run() {
